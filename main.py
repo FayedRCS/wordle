7a45614cab5f5186017d729433ed3f1c9f5b5423
fayed <fayed_rana@outlook.com>
2024-09-10 03:11:13 +0200

    working game and mechanics

--- main.py
+++ main.py
@@ -14,39 +14,63 @@ def random_word():
 
     return wordle
 
-def guess():
+def guess(wordled, incorrect):
         
-        guess = str(input("guess a letter: "))
-        assert len(guess) == 5, ("Please guess a 5 letter word, or u lose motherfucker")
-
+        #Making sure it's a 5 letter word
+        valid_guess = False
+        while valid_guess == False:
+            guess = str(input("Guess a 5 letter word: "))
+            if len(guess) == 5:
+                 valid_guess = True
+                 break
+            print("Invalid answer dummy")
+            
+        #Making the guess into a list we can compare to
+    
         letters = []
         for letter in guess:
             letters.append(letter)
 
-        return letters
+        right_in_wordle = []
 
+        for i in range(len(letters)):
+            compare = letters[i]
+            if compare == wordled[i]:
+                right_in_wordle.append(compare.upper())
+                letters[i] = None # mark this letter as used
+            elif compare in wordled:
+                right_in_wordle.append(compare)
+            else:
+                right_in_wordle.append("x")
+                incorrect.add(compare)
 
-def matching(lst):
-
-    win_word = lst
-    guessed_word = guess()
-    print(win_word)
-
-        #(['p', 'e', 'a', 'c', 'e'], ['j', 'a', 'm', 'e', 's']) #output
-
-    for i in win_word:
-         for j in guessed_word:
-              if j == i:
-                print("v nice")
-
-    pass
-
+    
+        return right_in_wordle, "".join(wordled), guess.lower()
 
 def main():
+
     guesses = 0
-    matching(random_word())
+    wordle = random_word()
+    max_guesses = 6
+    incorrect = set() #making a set to avoid duplicates easily
 
-    while guesses < 2:
+    answer = []
+    for i in wordle:
+        answer.append(i)
+
+    print(wordle)
+
+    while guesses < max_guesses:
+        print(f"Guess #{guesses + 1} of {max_guesses}")
+        right_in_wordle, wordle, user_guess = guess(wordle, incorrect)
+
+        print(f"incorrect letters: {incorrect}")
+        print(right_in_wordle)
+
+        if user_guess.lower() == wordle.lower():
+            print(f"You got it, king! The word was {wordle}")
+            return
+        
         guesses += 1
-
+        
 main()
